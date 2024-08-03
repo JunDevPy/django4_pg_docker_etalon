57ec3294926a940db54e6a049f5492b6debde7aa
JunDevPy <111221302+JunDevPy@users.noreply.github.com>
2024-08-03 11:40:51 +0300

    Update urls.py

--- web_site/web_site/urls.py
+++ web_site/web_site/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home_page_site.urls'))
 ]
+
+admin.site.site_header = "Панель управления сайтом"
+admin.site.index_title = "Название сайта ---- нужно писать сюда"
