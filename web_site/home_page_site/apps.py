e8f05cd1e14a120c0fd42f1229223c6e5b1cfc00
JunDevPy <111221302+JunDevPy@users.noreply.github.com>
2024-08-03 11:41:19 +0300

    Update apps.py

--- web_site/home_page_site/apps.py
+++ web_site/home_page_site/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class HomePageSiteConfig(AppConfig):
+    verbose_name = 'Главная страница сайта --- настройки'
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home_page_site'
